7b41b7515649d36fee7eedba6a7250d234b79717
SangemAbhinay <sangemabhinay18@gmail.com>
2026-01-16 08:46:50 +0530

    set axios

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
-      ? "http://localhost:3000"
-      : import.meta.env.VITE_API_URL,
+      ? "http://localhost:3000/api"
+      : "https://chatify-7om8.onrender.com/api",
   withCredentials: true,
 });
